db620d54d851eb31488fcaf5e447c024a51b8921
Gedielson Vieira <gedielsonvieira@outlook.com>
2023-09-14 21:43:19 -0300

    created domain

---
+++ src/main/java/com/carrental/domain/model/CarAccessories.java
@@ -0,0 +1,11 @@
+package com.carrental.domain.model;
+
+public class CarAccessories {
+    private int quantityOfDoors;
+    private boolean hydraulicSteering;
+    private boolean airConditioning;
+    private boolean highlight;
+    private boolean electricGlass;
+    private boolean airBag;
+    private boolean ABS;
+}
