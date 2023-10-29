1dd66a769e4027e2eca410d7bf65e936dd5ce831
Gedielson Vieira <gedielsonvieira@outlook.com>
2023-10-29 20:18:08 -0300

    add the type to the specification

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO table_car (marca, nome, tipo, imagem_url, destaque, descricao, preco, ano, data_criacao, numero_pessoas, quantidade_portas,
-                       direcao_hidraulica, ar_condicionado, vidro_eletrico, airbag, abs)
-VALUES ('Fiat', 'Uno', 'Hatch','image-src',true, 'descricao', 1500, 2000,'2023-09-20', 4, 2, false, false, false, false, false);
+INSERT INTO table_car (marca, nome, tipo, imagem_url, destaque, descricao, preco, ano, data_criacao, numero_pessoas,
+                       quantidade_portas,direcao_hidraulica, ar_condicionado, vidro_eletrico, airbag, abs)
+VALUES ('Fiat', 'Uno', 'Hatch','image-src',true, 'descricao', 1500, 2000,'2023-09-20', 4, 2, false, false, false,
+        false, false);
